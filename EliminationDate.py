7405edaeaf998539f486836bc078313435a7c3ca
Yuchen Yue <yue.physics@gmail.com>
2017-07-26 19:20:08 -0400

    get first result

--- EliminationDate.py
+++ EliminationDate.py
@@ -19,7 +19,7 @@ def Find_EDate(team,dates,matches,daily_scores,daily_rank_board):
 		for i in range(1,Num_of_dates):
 			date = dates[Num_of_dates-i]
 			BF_Rank = Best_Final_Rank(team,date,dates,matches,daily_scores,daily_rank_board)
-			print BF_Rank[team]
+			#print BF_Rank[team]
 			if BF_Rank[team] < 9:
 				return date
 			elif i == Num_of_dates-1:
@@ -29,14 +29,14 @@ def Find_EDate(team,dates,matches,daily_scores,daily_rank_board):
 def Best_Final_Rank(team,date,dates,matches,daily_scores,daily_rank_board):
 	daily_score = {}
 	Num_of_date = dates.index(date)
-	for team in daily_rank_board[0].keys():
-		daily_score[team] = daily_scores[team][Num_of_date-1]
+	for teami in daily_rank_board[0].keys():
+		daily_score[teami] = daily_scores[teami][Num_of_date-1]
 
 
 	Daily_Rank = daily_rank_board[Num_of_date-1]
 	Best_score = daily_score
 	Best_Rank = Daily_Rank
-	#print Best_score
+	#print Best_score[team],team#, Best_Rank
 	while True:
 		if Num_of_date == len(dates):
 			break
@@ -45,7 +45,7 @@ def Best_Final_Rank(team,date,dates,matches,daily_scores,daily_rank_board):
 		daily_match = matches[Num_of_date]
 		Best_Rank, Best_score = Best_daily_Rank(team,teams,Best_Rank,Best_score,daily_match)
 		Num_of_date = Num_of_date + 1
-		#print Best_Rank
+		#print Best_score[team],team#, Best_Rank
 	return Best_Rank
 
 
@@ -53,18 +53,24 @@ def Best_daily_Rank(team,teams,Best_Rank,Best_score,daily_match):
 	conference = teams[team]
 	for match in daily_match.keys():
 		if match[0] == team:
-			Best_score[match[0]] = (Best_score[match[0]][0] + 1, Best_score[match[0]][1] )
+			Best_score[team] = (Best_score[team][0] + 1, Best_score[team][1] )
 			if teams[match[1]] == conference:
 				Best_score[match[1]] = (Best_score[match[1]][0], Best_score[match[1]][1] + 1)
 		elif match[1] == team:
-			Best_score[match[1]] = (Best_score[match[1]][0] + 1, Best_score[match[1]][1] )
+			Best_score[team] = (Best_score[team][0] + 1, Best_score[team][1] )
 			if teams[match[0]] == conference:
 				Best_score[match[0]] = (Best_score[match[0]][0], Best_score[match[0]][1] + 1)
 			
 		elif teams[match[0]] == conference and teams[match[1]] == conference:
 			r1 = Best_Rank[match[0]]
+			win1 = Best_score[match[0]][0]
+			lose1 = Best_score[match[0]][1]
 			r2 = Best_Rank[match[1]]
+			win2= Best_score[match[1]][0]
+			lose2 = Best_score[match[1]][1]
 			r = Best_Rank[team]
+			win = Best_score[team][0]
+			lose = Best_score[team][1]
 			if r1 < r2:
 				if r1 < r < r2 :
 					Best_score[match[1]] = (Best_score[match[1]][0] + 1, Best_score[match[1]][1] )
@@ -75,6 +81,14 @@ def Best_daily_Rank(team,teams,Best_Rank,Best_score,daily_match):
 				elif r1 < r2 < r :
 					Best_score[match[0]] = (Best_score[match[0]][0] + 1, Best_score[match[0]][1] )
 					Best_score[match[1]] = (Best_score[match[1]][0], Best_score[match[1]][1] + 1)
+				elif r == r1 :
+					Best_score[match[1]] = (Best_score[match[1]][0] + 1, Best_score[match[1]][1] )
+					Best_score[match[0]] = (Best_score[match[0]][0], Best_score[match[0]][1] + 1)
+				elif r == r2:
+					Best_score[match[0]] = (Best_score[match[0]][0] + 1, Best_score[match[0]][1] )
+					Best_score[match[1]] = (Best_score[match[1]][0], Best_score[match[1]][1] + 1)
+				
+
 
 			elif r2 < r1:
 				if r2 < r < r1 :
@@ -84,6 +98,12 @@ def Best_daily_Rank(team,teams,Best_Rank,Best_score,daily_match):
 					Best_score[match[0]] = (Best_score[match[0]][0] + 1, Best_score[match[0]][1] )
 					Best_score[match[1]] = (Best_score[match[1]][0], Best_score[match[1]][1] + 1)
 				elif r2 < r1 < r :
+					Best_score[match[1]] = (Best_score[match[1]][0] + 1, Best_score[match[1]][1] )
+					Best_score[match[0]] = (Best_score[match[0]][0], Best_score[match[0]][1] + 1)
+				elif r == r2:
+					Best_score[match[1]] = (Best_score[match[1]][0], Best_score[match[1]][1] + 1)
+					Best_score[match[0]] = (Best_score[match[0]][0] + 1, Best_score[match[0]][1])
+				elif r == r1:
 					Best_score[match[1]] = (Best_score[match[1]][0] + 1, Best_score[match[1]][1] )
 					Best_score[match[0]] = (Best_score[match[0]][0], Best_score[match[0]][1] + 1)
 
@@ -133,8 +153,9 @@ def Rank(Best_score):
 	return Best_Rank
 
 
-
-print (EliminationDate('Brooklyn Nets',dates,matches,daily_scores_east,daily_scores_west,daily_rank_board_east,daily_rank_board_west))
+for teami in teams:
+	print teami
+	print (EliminationDate(teami,dates,matches,daily_scores_east,daily_scores_west,daily_rank_board_east,daily_rank_board_west))
 
 
 
